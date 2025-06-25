b50babeeefeede416ce4666b77c099a00f2e29e5
hlum <72328049+corripio13@users.noreply.github.com>
2025-06-25 14:05:01 -0400

    Add files via upload

---
+++ ID_info_pages.py
@@ -0,0 +1,12 @@
+ip_dplmc_autoloot = 0
+ip_dplmc_autosell = 1
+ip_morale = 2
+ip_economy = 3
+ip_courtship = 4
+ip_politics = 5
+ip_character_backgrounds = 6
+ip_military_campaigns = 7
+ip_prebattle = 8
+ip_tactics = 9
+
+
